a9769e1766c41fc7604cca8d9e923e96e164cd1a
ThierrryScotto <thierry_192@outlook.com>
2021-05-03 21:02:10 -0300

    almost ready

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,10 +1,23 @@
+require_relative('../services/s3')
+
 class ClientController < ApplicationController
   def getFile
+    s3 = S3.new
+    puts s3.getFiles
+    
     render json: "File name is: #{params[:fileName]}"
   end
 
   def getAllFiles
-    render json: 'return all'
+    s3 = S3.new
+
+    begin
+      data = s3.getFiles()
+
+      render json: data, status: 200
+    rescue => exeption
+      render json "There was some error: #{exeption.class}", status: :bad_request
+    end
   end
 
   def sendFile
